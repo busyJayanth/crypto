2c5be1d80c958bd6db5272f17b77d619f87410a2
Jayanth_J <107485453+busyJayanth@users.noreply.github.com>
2023-09-26 15:10:05 +0530

    Create gunicorn_config.py

---
+++ gunicorn_config.py
@@ -0,0 +1,5 @@
+# gunicorn_config.py
+
+bind = "0.0.0.0:8000"  # Change this to the desired host and port
+workers = 4  # You can adjust the number of workers based on your server's capacity
+timeout = 120
